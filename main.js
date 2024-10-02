bd97a1f1690b8b4a51b85ef67fa73407facd5031
ivantarabanovych <vanjokcitet334@gmail.com>
2024-10-02 23:02:01 +0300

    q2

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-function delayedGreeting(name) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(`${name}`);
-    }, 3000);
-  });
+function randomReject() {
+    return new Promise((resolve, reject) => {
+        const randomTime = Math.floor(Math.random() * 5000) + 1000; 
+        const shouldResolve = Math.random() > 0.5; 
+
+        setTimeout(() => {
+            if (shouldResolve) {
+                const randomNumber = Math.floor(Math.random() * 100); 
+                resolve(randomNumber); 
+            } else {
+                reject(new Error("Виникла помилка")); 
+            }
+        }, randomTime);
+    });
 }
 
-delayedGreeting("Shmitt").then((greating) => {
-  console.log(greating);
-});
+randomReject()
+    .then((result) => {
+        console.log(`Обіцянка виконалася з результатом: ${result}`);
+    })
+    .catch((error) => {
+        console.error(`Обіцянка відхилена: ${error.message}`);
+    });
